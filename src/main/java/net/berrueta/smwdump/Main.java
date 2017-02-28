cff47a5c44d24b590577b38a47c4237722053bf9
Reto Gmür <me@farewellutopia.com>
2017-02-28 22:45:27 +0000

    Applied default sun formatting and configured checkstyle plugin

--- src/main/java/net/berrueta/smwdump/Main.java
+++ src/main/java/net/berrueta/smwdump/Main.java
@@ -1,4 +1,5 @@
 package net.berrueta.smwdump;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
@@ -21,118 +22,116 @@ import com.hp.hpl.jena.rdf.model.RDFNode;
 import com.hp.hpl.jena.rdf.model.Resource;
 import com.hp.hpl.jena.shared.JenaException;
 
-
 /**
- * Script to extract a RDF dump from a SemanticMediaWiki instance by
- * means of web requests, i.e., without having local access to the server
- * or the database.
- * 
+ * Script to extract a RDF dump from a SemanticMediaWiki instance by means of
+ * web requests, i.e., without having local access to the server or the
+ * database.
+ *
  * Command line syntax: wikiURL [outputfile.rdf]
- * 
+ *
  * The wikiURL parameter should be the URL of the wiki, i.e.,
  * http://www.crisiswiki.org/
- * 
- * The second parameter is optional. If specified, the collected
- * RDF triples will be stored in that file. Otherwise, the
- * file "out.rdf" is assumed.
- * 
+ *
+ * The second parameter is optional. If specified, the collected RDF triples
+ * will be stored in that file. Otherwise, the file "out.rdf" is assumed.
+ *
  * @author Diego Berrueta
  *
  */
 public class Main {
-    
-    private static final Logger logger = Logger.getLogger(Main.class);
 
-    /**
-     * Milliseconds between requests
-     */
-    private static final long DELAY_MILIS = 0;
-    
-    /**
-     * @param args
-     */
-    public static void main(String[] args) throws Exception {
-        if (args.length < 1) {
-            System.err.println("Please provide the URL to the wiki as the first argument, e.g., http://www.crisiswiki.org/");
-        } else {
-            String wikiUrl = args[0];
-            File outputFile = new File(args.length == 2 ? args[1] : "out.rdf");
-            MediaWikiBot b = new MediaWikiBot (wikiUrl);
-            int count = 0;
-            Model m = ModelFactory.createDefaultModel();
-            LinkedList<String> articleNames = new LinkedList<String>();
-            for (int namespace : MediaWiki.NS_ALL) {
-                logger.info("Geting a list of all pages in namespace " + namespace); // see http://en.wikipedia.org/wiki/Wikipedia:Namespace
-                AllPageTitles apt = new AllPageTitles(b, namespace);
-                articleNames.addAll(asList(apt));
-            }
-            logger.info("There are " + articleNames.size() + " pages");
-            long startTime = System.currentTimeMillis();
-            for (String articleName : articleNames) {
-                logger.info("Getting RDF data for article (" + count + " of " + articleNames.size() + "): " + articleName);
-                readArticleIntoModel(m, wikiUrl, articleName);
-                logger.info("After reading [[" + articleName + "]], the model contains " + m.size() + " triples");
-                count++;
-                long currentTime = System.currentTimeMillis();
-                double progress = (double) count / (double) articleNames.size();
-                long elapsedTime = currentTime-startTime;
-                long remainingTime = Math.round(elapsedTime * (1-progress) / progress);
-                logger.info("Elapsed time (sec): " + (currentTime-startTime)/1000 + " -- Progress: " + Math.floor(progress * 100.0) + "% -- Est. remaining time (sec): " + remainingTime/1000);
-                Thread.sleep(DELAY_MILIS);
-            }
-            removeMalformedURIs(m);
-            // save data
-            logger.info("Saving " + m.size() + " triples to file " + outputFile + ", " + count + " pages have been retrieved");
-            m.write(new FileOutputStream(outputFile)); // avoid FileWriter, see http://jena.sourceforge.net/IO/iohowto.html#encoding
-        }
+  private static final Logger logger = Logger.getLogger(Main.class);
+
+  /**
+   * Milliseconds between requests
+   */
+  private static final long DELAY_MILIS = 0;
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) throws Exception {
+    if (args.length < 1) {
+      System.err.println("Please provide the URL to the wiki as the first argument, e.g., http://www.crisiswiki.org/");
+    } else {
+      String wikiUrl = args[0];
+      File outputFile = new File(args.length == 2 ? args[1] : "out.rdf");
+      MediaWikiBot b = new MediaWikiBot(wikiUrl);
+      int count = 0;
+      Model m = ModelFactory.createDefaultModel();
+      LinkedList<String> articleNames = new LinkedList<String>();
+      for (int namespace : MediaWiki.NS_ALL) {
+        logger.info("Geting a list of all pages in namespace " + namespace); // see http://en.wikipedia.org/wiki/Wikipedia:Namespace
+        AllPageTitles apt = new AllPageTitles(b, namespace);
+        articleNames.addAll(asList(apt));
+      }
+      logger.info("There are " + articleNames.size() + " pages");
+      long startTime = System.currentTimeMillis();
+      for (String articleName : articleNames) {
+        logger.info("Getting RDF data for article (" + count + " of " + articleNames.size() + "): " + articleName);
+        readArticleIntoModel(m, wikiUrl, articleName);
+        logger.info("After reading [[" + articleName + "]], the model contains " + m.size() + " triples");
+        count++;
+        long currentTime = System.currentTimeMillis();
+        double progress = (double) count / (double) articleNames.size();
+        long elapsedTime = currentTime - startTime;
+        long remainingTime = Math.round(elapsedTime * (1 - progress) / progress);
+        logger.info("Elapsed time (sec): " + (currentTime - startTime) / 1000 + " -- Progress: " + Math.floor(progress * 100.0) + "% -- Est. remaining time (sec): " + remainingTime / 1000);
+        Thread.sleep(DELAY_MILIS);
+      }
+      removeMalformedURIs(m);
+      // save data
+      logger.info("Saving " + m.size() + " triples to file " + outputFile + ", " + count + " pages have been retrieved");
+      m.write(new FileOutputStream(outputFile)); // avoid FileWriter, see http://jena.sourceforge.net/IO/iohowto.html#encoding
     }
+  }
 
-    private static LinkedList<String> asList(AllPageTitles apt) {
-        LinkedList<String> articleNames = new LinkedList<String>();
-        for (String articleName : apt) {
-            articleNames.add(articleName);
-        }
-        return articleNames;
+  private static LinkedList<String> asList(AllPageTitles apt) {
+    LinkedList<String> articleNames = new LinkedList<String>();
+    for (String articleName : apt) {
+      articleNames.add(articleName);
     }
+    return articleNames;
+  }
 
-    /**
-     * Fetches the RDF triples about a wiki article and adds them to the model
-     * 
-     * @param m
-     * @param articleName
-     */
-    private static void readArticleIntoModel(Model m, String wikiUrl, String articleName) {
-        String rdfUrl = wikiUrl + "index.php?title=Special:ExportRDF/" + MediaWiki.encode(articleName);
-        logger.debug("RDF URL: " + rdfUrl);
+  /**
+   * Fetches the RDF triples about a wiki article and adds them to the model
+   *
+   * @param m
+   * @param articleName
+   */
+  private static void readArticleIntoModel(Model m, String wikiUrl, String articleName) {
+    String rdfUrl = wikiUrl + "index.php?title=Special:ExportRDF/" + MediaWiki.encode(articleName);
+    logger.debug("RDF URL: " + rdfUrl);
+    try {
+      m.read(rdfUrl);
+    } catch (JenaException e) {
+      logger.error("Skipped " + rdfUrl + " because of parsing errors", e);
+    }
+  }
+
+  /**
+   * Remove buggy resource URIs, i.e., URIs that are not valid
+   *
+   * @param m
+   */
+  private static void removeMalformedURIs(Model m) {
+    IRIFactory iriFactory = IRIFactory.semanticWebImplementation();
+    NodeIterator nodeIterator = m.listObjects();
+    while (nodeIterator.hasNext()) {
+      RDFNode node = nodeIterator.next();
+      if (node.isResource() == true && node.isAnon() == false) {
+        Resource resource = node.asResource();
+        logger.info("Checking " + resource.getURI());
         try {
-            m.read(rdfUrl);
-        } catch (JenaException e) {
-            logger.error("Skipped " + rdfUrl + " because of parsing errors", e);
+          iriFactory.construct(resource.getURI()); // just try to construct the IRI, check for exceptions
+        } catch (IRIImplException e) {
+          logger.error("Malformed URI fetched from wiki: " + resource.getURI());
+          logger.info("Removing all triples with object: " + resource.getURI());
+          m.removeAll(null, null, resource);
         }
+      }
     }
-
-    /**
-     * Remove buggy resource URIs, i.e., URIs that are not valid
-     * 
-     * @param m
-     */
-    private static void removeMalformedURIs(Model m) {
-        IRIFactory iriFactory = IRIFactory.semanticWebImplementation();
-        NodeIterator nodeIterator = m.listObjects();
-        while (nodeIterator.hasNext()) {
-            RDFNode node = nodeIterator.next();
-            if (node.isResource() == true && node.isAnon() == false) {
-                Resource resource = node.asResource();
-                logger.info("Checking "  + resource.getURI());
-                try {
-                    iriFactory.construct(resource.getURI()); // just try to construct the IRI, check for exceptions
-                } catch (IRIImplException e) {
-                    logger.error("Malformed URI fetched from wiki: " + resource.getURI());
-                    logger.info("Removing all triples with object: " + resource.getURI());
-                    m.removeAll(null, null, resource);
-                }
-            }
-        }
-    }
+  }
 
 }
